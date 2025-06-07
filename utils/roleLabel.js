42711e0b75136783401a0712f22b0f54efe0adb1
Bet-Zero <driertoaster1@gmail.com>
2025-06-07 08:52:18 -0400

    Refactor common logic into utils

---
+++ utils/roleLabel.js
@@ -0,0 +1,16 @@
+export default function getPlayerPositionLabel(fullPosition) {
+  const map = {
+    Guard: 'G',
+    'Point Guard': 'PG',
+    'Shooting Guard': 'SG',
+    Forward: 'F',
+    'Small Forward': 'SF',
+    'Power Forward': 'PF',
+    Center: 'C',
+    'Forward-Center': 'F/C',
+    'Guard-Forward': 'G/F',
+    'Forward-Guard': 'F',
+    'Center-Forward': 'C',
+  };
+  return map[fullPosition] || fullPosition || '—';
+}
