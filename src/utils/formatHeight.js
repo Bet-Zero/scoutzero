f24156259c87f1828d8ec7b35d4bcd1634fbec49
Bet_Zero <driertoaster1@gmail.com>
2025-06-08 06:09:34 -0400

    refactor: Active Filter Display

---
+++ src/utils/formatHeight.js
@@ -0,0 +1,6 @@
+export default function formatHeight(inches = 0) {
+  if (inches === 0 || inches === null || inches === undefined) return '0\'0"';
+  const feet = Math.floor(inches / 12);
+  const remainingInches = inches % 12;
+  return `${feet}'${remainingInches}"`;
+}
