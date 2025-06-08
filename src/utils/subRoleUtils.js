01f070cde192efc99de06fe3f522d450d561888d
Bet_Zero <driertoaster1@gmail.com>
2025-06-08 05:00:48 -0400

    refactor: Role Filters

--- src/utils/subRoleUtils.js
+++ src/utils/subRoleUtils.js
@@ -4,3 +4,17 @@ export const isPositiveSubRole = (roleName) => {
   const role = SubRoleMasterList.find((r) => r.name === roleName);
   return role?.isPositive;
 };
+
+export const toggleSubroleSelection = (subRoles = {}, roleName) => {
+  const roleData = SubRoleMasterList.find((r) => r.name === roleName);
+  if (!roleData) return subRoles;
+
+  const { type } = roleData;
+  const currentList = subRoles[type] || [];
+  return {
+    ...subRoles,
+    [type]: currentList.includes(roleName)
+      ? currentList.filter((r) => r !== roleName)
+      : [...currentList, roleName],
+  };
+};
