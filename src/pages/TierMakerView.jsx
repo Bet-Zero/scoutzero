903b9fa62d84dcafa895100857686430f27b46c6
Bet_Zero <driertoaster1@gmail.com>
2025-07-05 06:01:33 -0400

    Your commit message here

--- src/pages/TierMakerView.jsx
+++ src/pages/TierMakerView.jsx
@@ -5,7 +5,7 @@ import TierMakerBoard from '@/features/tierMaker/TierMakerBoard';
 const TierMakerView = () => {
   const { tierListId } = useParams();
   return (
-    <div className="bg-neutral-900 min-h-screen text-white">
+    <div className="bg-neutral-900 min-h-screen text-white pt-4 pb-8">
       <TierMakerBoard initialTierListId={tierListId} />
     </div>
   );
