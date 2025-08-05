d70fde3c1c43ed48869597ca097bd350e9cf5b74
Bet-Zero <driertoaster1@gmail.com>
2025-08-04 23:01:01 -0500

    Update ContractEditorModal.jsx
    Dialog fix

--- src/features/architect/ContractEditorModal.jsx
+++ src/features/architect/ContractEditorModal.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dialog, DialogContent } from '@/components/shared/ui/dialog';
+import { Dialog, DialogContent } from '@/components/shared/ui/Dialog';
 import ContractEditor from './ContractEditor';
 
 const ContractEditorModal = ({
