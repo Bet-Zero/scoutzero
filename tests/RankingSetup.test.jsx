ca50fc40fe45ebd6455b21f6d48452adaf14cce4
Bet_Zero <driertoaster1@gmail.com>
2025-08-05 22:32:31 -0400

    New Update

--- tests/RankingSetup.test.jsx
+++ tests/RankingSetup.test.jsx
@@ -13,7 +13,7 @@ describe('RankingSetup', () => {
     render(<RankingSetup playerPool={samplePlayers} onComplete={handle} />);
     const top = screen.getByTestId('top-tier');
     fireEvent.click(within(top).getByText('Alpha'));
-    fireEvent.click(screen.getByText('Ready to Rank'));
+    fireEvent.click(screen.getByText('Start'));
     expect(handle).toHaveBeenCalledWith({
       topTier: ['1'],
       bottomTier: [],
