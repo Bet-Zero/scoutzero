fff2e56a910891b6a04ba7686fbbec800aab1acd
Bet_Zero <driertoaster1@gmail.com>
2026-04-18 02:55:15 -0400

    refactor: convert teamColors to TypeScript

--- src/shared/utils/formatting/teamColors.ts
+++ src/shared/utils/formatting/teamColors.ts
@@ -1,17 +1,26 @@
-// src/utils/formatting/teamColors.js
+export type TeamClassSet = {
+  bg: string;
+  text: string;
+  border: string;
+};
 
-export const TEAM_COLOR_MAP = {
+export type TeamColors = {
+  primary: string;
+  secondary: string;
+  tertiary: string;
+  text: string;
+  bg: string;
+  classSet: TeamClassSet;
+};
+
+export const TEAM_COLOR_MAP: Record<string, TeamColors> = {
   hawks: {
     primary: '#E03A3E',
     secondary: '#C1D32F',
     tertiary: '#000000',
     text: '#FFFFFF',
     bg: '#E03A3E',
-    classSet: {
-      bg: 'bg-[#E03A3E]',
-      text: 'text-[#C1D32F]',
-      border: 'border-[#C1D32F]',
-    },
+    classSet: { bg: 'bg-[#E03A3E]', text: 'text-[#C1D32F]', border: 'border-[#C1D32F]' },
   },
   celtics: {
     primary: '#007A33',
@@ -19,11 +28,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#000000',
     text: '#FFFFFF',
     bg: '#007A33',
-    classSet: {
-      bg: 'bg-[#007A33]',
-      text: 'text-[#BA9653]',
-      border: 'border-[#BA9653]',
-    },
+    classSet: { bg: 'bg-[#007A33]', text: 'text-[#BA9653]', border: 'border-[#BA9653]' },
   },
   nets: {
     primary: '#000000',
@@ -31,11 +36,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#C4CED4',
     text: '#FFFFFF',
     bg: '#1C1C1C',
-    classSet: {
-      bg: 'bg-black',
-      text: 'text-white',
-      border: 'border-white',
-    },
+    classSet: { bg: 'bg-black', text: 'text-white', border: 'border-white' },
   },
   hornets: {
     primary: '#1D1160',
@@ -43,11 +44,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#A1A1A4',
     text: '#FFFFFF',
     bg: '#1D1160',
-    classSet: {
-      bg: 'bg-[#1D1160]',
-      text: 'text-[#00B1C1]',
-      border: 'border-[#00788C]',
-    },
+    classSet: { bg: 'bg-[#1D1160]', text: 'text-[#00B1C1]', border: 'border-[#00788C]' },
   },
   bulls: {
     primary: '#CE1141',
@@ -55,11 +52,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#C4CED4',
     text: '#FFFFFF',
     bg: '#6A0E25',
-    classSet: {
-      bg: 'bg-[#CE1141]',
-      text: 'text-white',
-      border: 'border-white',
-    },
+    classSet: { bg: 'bg-[#CE1141]', text: 'text-white', border: 'border-white' },
   },
   cavaliers: {
     primary: '#6F263D',
@@ -67,11 +60,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#041E42',
     text: '#FFFFFF',
     bg: '#6F263D',
-    classSet: {
-      bg: 'bg-[#6F263D]',
-      text: 'text-[#FFB81C]',
-      border: 'border-[#FFB81C]',
-    },
+    classSet: { bg: 'bg-[#6F263D]', text: 'text-[#FFB81C]', border: 'border-[#FFB81C]' },
   },
   mavericks: {
     primary: '#00538C',
@@ -79,11 +68,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#002B5E',
     text: '#FFFFFF',
     bg: '#002B5E',
-    classSet: {
-      bg: 'bg-[#00538C]',
-      text: 'text-[#B8C4CA]',
-      border: 'border-[#B8C4CA]',
-    },
+    classSet: { bg: 'bg-[#00538C]', text: 'text-[#B8C4CA]', border: 'border-[#B8C4CA]' },
   },
   nuggets: {
     primary: '#0E2240',
@@ -91,11 +76,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#8B2131',
     text: '#FFFFFF',
     bg: '#0E2240',
-    classSet: {
-      bg: 'bg-[#0E2240]',
-      text: 'text-[#FEC524]',
-      border: 'border-[#FEC524]',
-    },
+    classSet: { bg: 'bg-[#0E2240]', text: 'text-[#FEC524]', border: 'border-[#FEC524]' },
   },
   pistons: {
     primary: '#C8102E',
@@ -103,11 +84,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#BEC0C2',
     text: '#FFFFFF',
     bg: '#C8102E',
-    classSet: {
-      bg: 'bg-[#C8102E]',
-      text: 'text-[#006BB6]',
-      border: 'border-[#006BB6]',
-    },
+    classSet: { bg: 'bg-[#C8102E]', text: 'text-[#006BB6]', border: 'border-[#006BB6]' },
   },
   warriors: {
     primary: '#1D428A',
@@ -115,11 +92,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#000000',
     text: '#FFFFFF',
     bg: '#1D428A',
-    classSet: {
-      bg: 'bg-[#1D428A]',
-      text: 'text-[#FFC72C]',
-      border: 'border-[#FFC72C]',
-    },
+    classSet: { bg: 'bg-[#1D428A]', text: 'text-[#FFC72C]', border: 'border-[#FFC72C]' },
   },
   rockets: {
     primary: '#CE1141',
@@ -127,11 +100,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#000000',
     text: '#FFFFFF',
     bg: '#8A0C24',
-    classSet: {
-      bg: 'bg-[#CE1141]',
-      text: 'text-[#C4CED4]',
-      border: 'border-[#C4CED4]',
-    },
+    classSet: { bg: 'bg-[#CE1141]', text: 'text-[#C4CED4]', border: 'border-[#C4CED4]' },
   },
   pacers: {
     primary: '#002D62',
@@ -139,11 +108,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#BEC0C2',
     text: '#FFFFFF',
     bg: '#002D62',
-    classSet: {
-      bg: 'bg-[#002D62]',
-      text: 'text-[#FDBB30]',
-      border: 'border-[#FDBB30]',
-    },
+    classSet: { bg: 'bg-[#002D62]', text: 'text-[#FDBB30]', border: 'border-[#FDBB30]' },
   },
   clippers: {
     primary: '#C8102E',
@@ -151,11 +116,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#BEC0C2',
     text: '#FFFFFF',
     bg: '#C8102E',
-    classSet: {
-      bg: 'bg-[#C8102E]',
-      text: 'text-[#1D428A]',
-      border: 'border-[#1D428A]',
-    },
+    classSet: { bg: 'bg-[#C8102E]', text: 'text-[#1D428A]', border: 'border-[#1D428A]' },
   },
   lakers: {
     primary: '#552583',
@@ -163,11 +124,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#000000',
     text: '#FFFFFF',
     bg: '#28104E',
-    classSet: {
-      bg: 'bg-[#552583]',
-      text: 'text-[#FDB927]',
-      border: 'border-[#FDB927]',
-    },
+    classSet: { bg: 'bg-[#552583]', text: 'text-[#FDB927]', border: 'border-[#FDB927]' },
   },
   grizzlies: {
     primary: '#5D76A9',
@@ -175,11 +132,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#707271',
     text: '#FFFFFF',
     bg: '#12173F',
-    classSet: {
-      bg: 'bg-[#5D76A9]',
-      text: 'text-[#CACCCE]',
-      border: 'border-[#5D76A9]',
-    },
+    classSet: { bg: 'bg-[#5D76A9]', text: 'text-[#CACCCE]', border: 'border-[#5D76A9]' },
   },
   heat: {
     primary: '#98002E',
@@ -187,11 +140,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#000000',
     text: '#FFFFFF',
     bg: '#490013',
-    classSet: {
-      bg: 'bg-[#98002E]',
-      text: 'text-[#F9A01B]',
-      border: 'border-[#F9A01B]',
-    },
+    classSet: { bg: 'bg-[#98002E]', text: 'text-[#F9A01B]', border: 'border-[#F9A01B]' },
   },
   bucks: {
     primary: '#00471B',
@@ -199,11 +148,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#007A33',
     text: '#FFFFFF',
     bg: '#003014',
-    classSet: {
-      bg: 'bg-[#00471B]',
-      text: 'text-[#EEE1C6]',
-      border: 'border-[#EEE1C6]',
-    },
+    classSet: { bg: 'bg-[#00471B]', text: 'text-[#EEE1C6]', border: 'border-[#EEE1C6]' },
   },
   timberwolves: {
     primary: '#0C2340',
@@ -211,11 +156,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#9EA2A2',
     text: '#FFFFFF',
     bg: '#0C2340',
-    classSet: {
-      bg: 'bg-[#0C2340]',
-      text: 'text-[#9EA2A2]',
-      border: 'border-[#236192]',
-    },
+    classSet: { bg: 'bg-[#0C2340]', text: 'text-[#9EA2A2]', border: 'border-[#236192]' },
   },
   pelicans: {
     primary: '#0C2340',
@@ -223,11 +164,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#85714D',
     text: '#FFFFFF',
     bg: '#0C2340',
-    classSet: {
-      bg: 'bg-[#0C2340]',
-      text: 'text-[#C8102E]',
-      border: 'border-[#C8102E]',
-    },
+    classSet: { bg: 'bg-[#0C2340]', text: 'text-[#C8102E]', border: 'border-[#C8102E]' },
   },
   knicks: {
     primary: '#F58426',
@@ -235,11 +172,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#BEC0C2',
     text: '#FFFFFF',
     bg: '#1B365D',
-    classSet: {
-      bg: 'bg-[#F58426]',
-      text: 'text-[#006BB6]',
-      border: 'border-[#006BB6]',
-    },
+    classSet: { bg: 'bg-[#F58426]', text: 'text-[#006BB6]', border: 'border-[#006BB6]' },
   },
   thunder: {
     primary: '#007AC1',
@@ -247,11 +180,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#FDBB30',
     text: '#FFFFFF',
     bg: '#007AC1',
-    classSet: {
-      bg: 'bg-[#007AC1]',
-      text: 'text-[#EF3B24]',
-      border: 'border-[#EF3B24]',
-    },
+    classSet: { bg: 'bg-[#007AC1]', text: 'text-[#EF3B24]', border: 'border-[#EF3B24]' },
   },
   magic: {
     primary: '#0077C0',
@@ -259,11 +188,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#000000',
     text: '#FFFFFF',
     bg: '#003DA5',
-    classSet: {
-      bg: 'bg-[#0077C0]',
-      text: 'text-[#C4CED4]',
-      border: 'border-[#C4CED4]',
-    },
+    classSet: { bg: 'bg-[#0077C0]', text: 'text-[#C4CED4]', border: 'border-[#C4CED4]' },
   },
   '76ers': {
     primary: '#006BB6',
@@ -271,11 +196,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#002B5C',
     text: '#FFFFFF',
     bg: '#002B5C',
-    classSet: {
-      bg: 'bg-[#006BB6]',
-      text: 'text-[#ED174C]',
-      border: 'border-[#ED174C]',
-    },
+    classSet: { bg: 'bg-[#006BB6]', text: 'text-[#ED174C]', border: 'border-[#ED174C]' },
   },
   suns: {
     primary: '#1D1160',
@@ -283,11 +204,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#F9A01B',
     text: '#FFFFFF',
     bg: '#1D1160',
-    classSet: {
-      bg: 'bg-[#1D1160]',
-      text: 'text-[#E56020]',
-      border: 'border-[#E56020]',
-    },
+    classSet: { bg: 'bg-[#1D1160]', text: 'text-[#E56020]', border: 'border-[#E56020]' },
   },
   blazers: {
     primary: '#E03A3E',
@@ -295,11 +212,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#C4CED4',
     text: '#FFFFFF',
     bg: '#E03A3E',
-    classSet: {
-      bg: 'bg-[#E03A3E]',
-      text: 'text-white',
-      border: 'border-white',
-    },
+    classSet: { bg: 'bg-[#E03A3E]', text: 'text-white', border: 'border-white' },
   },
   trailblazers: {
     primary: '#E03A3E',
@@ -307,11 +220,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#C4CED4',
     text: '#FFFFFF',
     bg: '#E03A3E',
-    classSet: {
-      bg: 'bg-[#E03A3E]',
-      text: 'text-white',
-      border: 'border-white',
-    },
+    classSet: { bg: 'bg-[#E03A3E]', text: 'text-white', border: 'border-white' },
   },
   kings: {
     primary: '#5A2D81',
@@ -319,11 +228,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#000000',
     text: '#FFFFFF',
     bg: '#2C1A4D',
-    classSet: {
-      bg: 'bg-[#5A2D81]',
-      text: 'text-[#A1A1A4]',
-      border: 'border-[#A1A1A4]',
-    },
+    classSet: { bg: 'bg-[#5A2D81]', text: 'text-[#A1A1A4]', border: 'border-[#A1A1A4]' },
   },
   spurs: {
     primary: '#C4CED4',
@@ -331,11 +236,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#9EA2A2',
     text: '#000000',
     bg: '#C4CED4',
-    classSet: {
-      bg: 'bg-[#C4CED4]',
-      text: 'text-black',
-      border: 'border-black',
-    },
+    classSet: { bg: 'bg-[#C4CED4]', text: 'text-black', border: 'border-black' },
   },
   raptors: {
     primary: '#CE1141',
@@ -343,11 +244,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#C4CED4',
     text: '#FFFFFF',
     bg: '#84001D',
-    classSet: {
-      bg: 'bg-[#CE1141]',
-      text: 'text-white',
-      border: 'border-white',
-    },
+    classSet: { bg: 'bg-[#CE1141]', text: 'text-white', border: 'border-white' },
   },
   jazz: {
     primary: '#002B5C',
@@ -355,11 +252,7 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#BEC0C2',
     text: '#FFFFFF',
     bg: '#002B5C',
-    classSet: {
-      bg: 'bg-[#002B5C]',
-      text: 'text-[#F9A01B]',
-      border: 'border-[#F9A01B]',
-    },
+    classSet: { bg: 'bg-[#002B5C]', text: 'text-[#F9A01B]', border: 'border-[#F9A01B]' },
   },
   wizards: {
     primary: '#002B5C',
@@ -367,32 +260,24 @@ export const TEAM_COLOR_MAP = {
     tertiary: '#C4CED4',
     text: '#FFFFFF',
     bg: '#002B5C',
-    classSet: {
-      bg: 'bg-[#002B5C]',
-      text: 'text-[#E31837]',
-      border: 'border-[#E31837]',
-    },
+    classSet: { bg: 'bg-[#002B5C]', text: 'text-[#E31837]', border: 'border-[#E31837]' },
   },
 };
 
-export function getTeamColors(teamName) {
+const DEFAULT_COLORS: TeamColors = {
+  primary: '#CCCCCC',
+  secondary: '#888888',
+  tertiary: '#444444',
+  text: '#FFFFFF',
+  bg: '#222222',
+  classSet: { bg: 'bg-gray-700', text: 'text-white', border: 'border-white' },
+};
+
+export function getTeamColors(teamName: string | null | undefined): TeamColors {
   const key = teamName?.toLowerCase?.().replace(/\s+/g, '') || '';
-  return (
-    TEAM_COLOR_MAP[key] || {
-      primary: '#CCCCCC',
-      secondary: '#888888',
-      tertiary: '#444444',
-      text: '#FFFFFF',
-      bg: '#222222',
-      classSet: {
-        bg: 'bg-gray-700',
-        text: 'text-white',
-        border: 'border-white',
-      },
-    }
-  );
+  return TEAM_COLOR_MAP[key] || DEFAULT_COLORS;
 }
 
-export function getTeamClassSet(teamName) {
+export function getTeamClassSet(teamName: string | null | undefined): TeamClassSet {
   return getTeamColors(teamName).classSet;
 }
